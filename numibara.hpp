55b6c8b53599ef36e67266763fe5fa55d0e7eca9
Marco Werner <werner.marco@outlook.de>
2023-05-25 12:48:52 +0200

    Updated the header file with include guards

--- numibara.hpp
+++ numibara.hpp
@@ -1,3 +1,10 @@
+#ifndef NUMIBARA
+#define NUMIBARA
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<string>
 #include "src/differentiation/differentiation.cpp"
 #include "src/integration/integration.cpp"
 #include "src/creation/creation.cpp"
+#endif
